5930f8b19352f0eef30a322fcd9331d46b419871
Kyle Houghton <kyle@keentocode.dev>
2023-06-01 20:57:46 -0500

    Routes configured and working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:index, :create, :update, :show, :destroy]
+  resources :users, only: [:index, :create, :update, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
