e6e2a75158d080f7f58143641272d53d7900262e
Kyle Houghton <kyle@keentocode.dev>
2023-06-03 21:30:38 -0500

    Migration to change users.password to users.password_hash.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_043124) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_022546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_043124) do
 
   create_table "users", force: :cascade do |t|
     t.text "email"
-    t.text "password"
+    t.text "password_hash"
     t.text "username"
     t.boolean "enabled"
     t.datetime "created_at", null: false
