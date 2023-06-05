62960a4e2df8875c95b9f05ada7f12d3debeba8a
Kyle Houghton <kyle@keentocode.dev>
2023-06-05 15:24:47 -0500

    Finished implementing password_hash for update User. Fixed extract! password code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,18 @@ class UsersController < ApplicationController
         validated_params = create_user_params.merge(enabled: true)
         password = validated_params.extract!(:password)
         new_user = User.new(validated_params)
-        new_user.password_hash = Password.create(password)
+        new_user.password_hash = Password.create(password[:password])
         new_user.save!
         render json: new_user.safe_attributes
     end
 
     def update
-        updated_user = User.update!(params[:id], update_user_params)
+        validated_params = update_user_params
+        password = validated_params.extract!(:password)
+        if password[:password] != nil
+            validated_params.merge!(password_hash: Password.create(password[:password]))
+        end
+        updated_user = User.update!(params[:id], validated_params)
         render json: updated_user.safe_attributes
     end
 
