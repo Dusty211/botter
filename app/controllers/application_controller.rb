f5386433c75e536b2bd1a072c8ced291cdf3234a
Kyle Houghton <kyle@keentocode.dev>
2023-06-04 22:00:57 -0500

    Implemented error handling pattern and error handling for ActiveRecord::RecordNotUnique

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+    rescue_from 'ActiveRecord::RecordNotUnique' do |exception|
+        render_error exception, 409
+    end
+    
+    private
+
+    def render_error(error, status_code)
+        render json: {error: error.class, message: error.message}, status: status_code
+    end
 end
