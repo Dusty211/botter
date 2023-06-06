8866b9786ab73eda7249a20d0e03e2598c5c4510
Kyle Houghton <kyle@keentocode.dev>
2023-06-06 00:25:23 -0500

    Fixed type in user#safe_attributes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    ONLY = [:id, :email, :username, :enabled, :created_ad, :updated_at]
+    ONLY = [:id, :email, :username, :enabled, :created_at, :updated_at]
 
     def safe_attributes
         self.as_json(:only => [*ONLY])
