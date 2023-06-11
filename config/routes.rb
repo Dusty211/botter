0574480b5fec49afa08528bcf7492360a6a6ba79
Kyle Houghton <kyle@keentocode.dev>
2023-06-10 20:25:39 -0500

    Added temp_links model and route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :create, :update, :show, :destroy]
   resources :users, only: [:create, :update, :show]
   resources :login, only: [:create]
+  resources :temp_links, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
