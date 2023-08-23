c69720de45d4a1d815e06783faae51c08651a8fa
Kyle Houghton <kyle@keentocode.dev>
2023-08-23 16:23:00 -0500

    Implemented stage specific env_vars.

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,8 @@ module Botter
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # custom env variable configuration:
+    config.env_variables = config_for(:env_variables)
   end
 end
